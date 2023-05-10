b0717b0cbb38dee9dee69ce9c3ea26a16c8d742e
nha236 <nguyenhuuan230601@gmail.com>
2023-05-10 12:02:55 +0700

    ++

--- ag4/ag4.js
+++ ag4/ag4.js
@@ -10,18 +10,36 @@ app.controller('contactController',function($scope){
             'name':'An',
             'email':'an@gmail.com',
             'phone':'0123456789'
-        },
-        {
-            id:1,
-            'name':'An',
-            'email':'an@gmail.com',
-            'phone':'0123456789'
-        },
-        {
-            id:2,
-            'name':'An',
-            'email':'an@gmail.com',
-            'phone':'0123456789'
         }
     ];
+    $scope.saveContact = function(){
+        if($scope.newcontact.id == null){
+            $scope.newcontact.id = uid++ ; 
+            $scope.contacts.push($scope.newcontact);
+        } else{
+            console.log('id not null');
+            for(i in $scope.contacts){
+                if($scope.contacts[i].id == $scope.newcontacts.id){
+                    $scope.contacts[i] = $scope.newcontact;
+                }
+            }
+        }
+        $scope.newcontact = {};
+    }
+    $scope.delete = function(id){
+        for (i in $scope.contacts){
+            if($scope.contacts[i].id == id){
+                $scope.contacts.splice(i,1);
+                $scope.newcontact=[]
+            }
+        }
+    }
+    $scope.edit = function(id){
+        for (i in $scope.contacts){
+            if($scope.contacts[i].id == id){
+                $scope.newcontact = angular.copy($scope.contacts[i])
+            }
+        }
+    }
+    
 });
